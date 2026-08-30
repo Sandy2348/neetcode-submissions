6a75bde931a7a86d5551fd4078dcff923636b070
Sandy2348 <154038795+Sandy2348@users.noreply.github.com>
2026-08-30 20:25:10 +0530

    Add: products-of-array-discluding-self - submission-8

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-8.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+       let product = new Array(nums.length);
+       let leftProduct = 1;
+       for(let i = 0; i <nums.length; i++){
+        product[i] = leftProduct;
+        leftProduct = leftProduct*nums[i]
+       }
+       let rightProduct = 1;
+       for(let i = nums.length-1; i>=0; i--){
+        product[i]= product[i]*rightProduct;
+        rightProduct = rightProduct*nums[i];
+       }
+       return product;
+    }
+}
