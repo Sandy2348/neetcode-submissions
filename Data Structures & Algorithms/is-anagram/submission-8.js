e3441e8face89bd282d80f98af40204aee342192
Sandy2348 <154038795+Sandy2348@users.noreply.github.com>
2026-09-02 08:04:14 +0530

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.js
@@ -0,0 +1,23 @@
+class Solution {
+    isAnagram(s, t) {
+        if(s.length!=t.length){
+            return false;
+        }
+      let sHash = new Map();
+      let tHash = new Map();
+      for(let ele of s){
+sHash.set(ele,(sHash.get(ele)||0)+1)
+      }
+      for(let ele of t){
+tHash.set(ele,(tHash.get(ele)||0)+1)
+      }
+      for(let [key]  of sHash){
+        if(sHash.get(key)!=tHash.get(key)){
+            return false;
+        }
+      }
+      return true;
+
+
+    }
+}
