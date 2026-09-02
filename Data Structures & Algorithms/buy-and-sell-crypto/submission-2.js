7a23da2ec781c72ff6594f40f039691841fbebe3
Sandy2348 <154038795+Sandy2348@users.noreply.github.com>
2026-09-02 07:50:51 +0530

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+     let left = 0;
+     let profit = 0;
+     //r = 10,l =10
+     for(let right = 0; right<prices.length; right++){
+        if(prices[right]<prices[left]){
+            left = right;
+        }
+        else{
+profit = Math.max(profit,prices[right]-prices[left])
+        }
+        
+     }
+     return profit;
+    }
+}
