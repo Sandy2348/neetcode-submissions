8d1a1b2d6ca6fc4ab2fa9a65d0fbdcfb66575714
Sandy2348 <154038795+Sandy2348@users.noreply.github.com>
2026-09-24 18:37:32 +0530

    Add: two-integer-sum-ii - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-9.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+       let leftPointer = 0;
+       let rightPointer = numbers.length-1;
+      while(leftPointer<rightPointer){
+        if(numbers[leftPointer]+numbers[rightPointer]== target){
+            return [leftPointer+1,rightPointer+1]
+        }
+        else if(numbers[leftPointer]+numbers[rightPointer]>target){
+            rightPointer--;
+        }
+        else if(numbers[leftPointer]+numbers[rightPointer]<target){
+            leftPointer++;
+        }
+      }
+
+    }}
